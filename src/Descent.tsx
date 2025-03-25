c4aebaebaf4614f677a25e1ec5aa0fd52fe08e8c
Carlton Joseph <carlton.joseph@gmail.com>
2025-03-25 13:25:49 -0700

    add descent text

---
+++ src/Descent.tsx
@@ -0,0 +1,10 @@
+export default function Approach() {
+  return (
+    <div>
+      Hike west along the summit ridge and then drop into a gully that leads
+      back south. Depending on your instinct and luck, moderate bushwacking may
+      be in order. Hike down the gully until given a chance to sidehill left.
+      You should end up right at your packs.
+    </div>
+  );
+}
